83d54033b195cf8dab5e58ff893966387edafb70
BaharKrc <144059055+BaharKrc@users.noreply.github.com>
2024-01-19 11:18:17 +0100

    del og mod

--- js02-operators/operators.js
+++ js02-operators/operators.js
@@ -73,3 +73,66 @@ console.log("a" * 3); // NaN
 
 console.log("5" * "9"); // 45. karakterler numeric ise numebr gibi davranarak işlemi gerçekleştiriyor.
 console.log(typeof ("5" * "9")); // number
+
+//del operatoru
+
+const del1 = 81;
+const del2 = 9;
+
+console.log(del1 / del2);
+console.log(del1 / "9");
+console.log(del1 / "dokuz"); //NaN
+
+console.log(del2 / 0); //infinity-sonsuzluk  ifadesi verir
+console.log(-61 / 0); //-infinity
+console.log(0 / 5); //0
+console.log(0 / -5);
+
+//modulus
+
+console.log(57 / 9);
+
+const mod1 = 57;
+const mod2 = 9;
+
+console.log(mod1 % mod2); //bølmedeki kalan degeerini dondurur
+console.log(27 / 2);
+
+const benimSayim = 967;
+const birlerBas = benimSayim % 10;
+console.log(birlerBas);
+
+const onlarBas = (benimSayim % 100) / 10;
+console.log(onlarBas);
+
+const yüzlerBas = benimSayim / 100; // 9.67
+console.log(yüzlerBas);
+
+//?Bazi hazır fonksiyonlar
+// Math.floor();  //* her zaman en yakin alt tam sayiya yuvarlar
+// Math.ceil();  //* her zaman en yakin ust tam sayiya yuvarlar
+// Math.trunc(); //* sayinin tam kismini alir.
+// Math.round(); //* en yakin tam sayiya yuvarlar.
+// Math.random(); //* 0 ve 1 arasında rasgele sayi uretir.
+// toFixed(basamakSayısı): // . dan sonra parantezin içine verdiğimiz sayı kadar basamak alır. Eğer noktadan sonraki basamak sayısı içine verdiğimiz sayıdan büyükse sondaki rakamı bir artırır.
+
+console.log(
+  `YüzlerBas: ${Math.trunc(yüzlerBas)}, OnlarBas: ${Math.trunc(
+    onlarBas
+  )}, birlerbas : ${birlerBas}`
+);
+
+const hazirSayi = 56.788888;
+
+console.log(`hazirSayi of Circle: ${hazirSayi.toFixed(3)} `); // 56.789
+console.log(`hazirSayi of Circle: ${Math.floor(hazirSayi)} `);
+console.log(`hazirSayi of Circle: ${Math.ceil(hazirSayi)} `);
+console.log(`hazirSayi of Circle: ${Math.trunc(hazirSayi)} `);
+console.log(`hazirSayi of Circle: ${Math.round(hazirSayi)} `);
+
+const randomNum = Math.random();
+console.log(randomNum);
+
+const randomNum2 = Math.random() * 10;
+console.log(randomNum2);
+console.log(Math.round(Math.random() * 10));
